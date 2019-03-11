b2063e24578a38ce5154538f3539ce61d315ef45
Faisal Alvi <faisalalvi33@gmail.com>
2019-03-11 15:38:56 +0530

    uploaded /src/index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloText from '../components/hellowworld.jsx';
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    ReactDOM.render(HelloText, document.getElementById('root'));
+    
+});
